2e42a70448abb60db8b869708c95fe8ac74bb179
RainingRetro <86686544+RainingRetro@users.noreply.github.com>
2021-07-02 19:52:26 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function red_bg()
+{
+  r = 255;
+  g = 0;
+  b = 0;
+}
+
+function green_bg()
+{
+  r = 0;
+  g = 255;
+  b = 0;
+}
+
+function setup() {
+  createCanvas(400, 400);  
+
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  bth_red.mousepressed(red_bg);
+
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
+}
+
+function draw() {
+  background(r,g,b);
+
+  drawSprites();
+}
+
+
+
+
+
+  
